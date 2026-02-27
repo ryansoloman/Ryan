682540b3e0acead419cc55bf4c0735cdfe11fbc1
ryansoloman <aptechryan@gmail.com>
2026-02-27 12:29:25 +0500

    Static Methods

---
+++ Ryan (1)/Semester2/AdvancedJs/static-methods.js
@@ -0,0 +1,34 @@
+class Lab08 {
+    constructor () {
+        console.log("Constructor Method");
+    }
+    test () {
+        console.log("This Is A Custom Method");
+    }
+    static specialMethod () {
+        console.log("This Is A Static Method");
+    }
+}
+
+const daniyal_1 = new Lab08();
+const daniyal_2 = new Lab08();
+const daniyal_3 = new Lab08();
+const daniyal_4 = new Lab08();
+const daniyal_5 = new Lab08();
+const daniyal_6 = new Lab08();
+const daniyal_7 = new Lab08();
+const daniyal_8 = new Lab08();
+const daniyal_9 = new Lab08();
+const daniyal_10 = new Lab08();
+daniyal_1.test();
+daniyal_2.test();
+daniyal_3.test();
+daniyal_4.test();
+daniyal_5.test();
+daniyal_6.test();
+daniyal_7.test();
+daniyal_8.test();
+daniyal_9.test();
+daniyal_10.test();
+
+Lab08.specialMethod();
